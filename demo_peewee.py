e4b2f5cd4226bd260d9aae04a17adf3f98c289d5
Kevin Samuel <contact@bitecode.dev>
2024-12-19 16:16:33 +0100

    feat: demo peewee

---
+++ demo_peewee.py
@@ -0,0 +1,16 @@
+import peewee as pw
+
+
+db = pw.SqliteDatabase("ecommerce.sqlite")
+
+
+class Product(pw.Model):
+    name = pw.CharField()
+    price = pw.FloatField()
+
+    class Meta:
+        database = db
+
+
+db.connect()
+db.create_tables([Product])
